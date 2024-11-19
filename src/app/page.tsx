185731f2c6b11b8d96dbc2e016823ed0994230ba
metacortexcypher <prithvi.chigurupati76@gmail.com>
2024-11-19 12:58:36 +0530

    extra button removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import NewspaperLayout from '@/components/NewspaperLayout';
-import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <>
       <NewspaperLayout />
-      <div className="text-center mt-8">
-        <Link href="/tally-form" className="bg-black text-white font-serif py-2 px-8 hover:bg-gray-800 transition-colors duration-200 border-2 border-black">
-          Join the Drama Early
-        </Link>
-      </div>
+      
+      
     </>
   );
 }
