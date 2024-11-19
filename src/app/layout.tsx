d37f0acfa4c7ce0d7905519395377b23cabbb9fb
metacortexcypher <prithvi.chigurupati76@gmail.com>
2024-11-19 21:17:27 +0530

    removed the git background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${playfair.className} grid-background antialiased`}>
+      <body className={`${playfair.className}`}>
         {children}
         <Analytics/>
       </body>
